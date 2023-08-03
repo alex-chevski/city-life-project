e23b3d1ad9e5e547006aea2cf6c964de4e0d17e1
frostep <frostell.matters@gmail.com>
2023-08-03 03:42:33 +0300

    add update routes

--- api/routes/breadcrumbs.php
+++ api/routes/breadcrumbs.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
+use App\Models\User\User;
 use DaveJamesMiller\Breadcrumbs\BreadcrumbsGenerator as Crumbs;
 use DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs;
+use Illuminate\Support\Str;
 
 Breadcrumbs::register('home', static function (Crumbs $crumbs): void {
     $crumbs->push('Главная страница', route('home'));
@@ -26,10 +28,37 @@ Breadcrumbs::register('password.request', function (Crumbs $crumbs): void {
 
 Breadcrumbs::register('password.reset', function (Crumbs $crumbs): void {
     $crumbs->parent('password.request');
-    $crumbs->push('Change', route('password.reset'));
+    $crumbs->push('', route('password.reset', Str::uuid()));
 });
 
 Breadcrumbs::register('cabinet', function (Crumbs $crumbs): void {
     $crumbs->parent('home');
     $crumbs->push('Кабинет', route('cabinet'));
+});
+
+// Admin
+Breadcrumbs::register('admin.home', function (Crumbs $crumbs): void {
+    $crumbs->parent('home');
+    $crumbs->push('Admin', route('admin.home'));
+});
+
+// Users
+Breadcrumbs::register('admin.users.index', function (Crumbs $crumbs): void {
+    $crumbs->parent('admin.home');
+    $crumbs->push('Users', route('admin.users.index'));
+});
+
+Breadcrumbs::register('admin.users.create', function (Crumbs $crumbs): void {
+    $crumbs->parent('admin.users.index');
+    $crumbs->push('Create', route('admin.users.create'));
+});
+
+Breadcrumbs::register('admin.users.show', function (Crumbs $crumbs, User $user): void {
+    $crumbs->parent('admin.users.index');
+    $crumbs->push($user->name, route('admin.users.show', $user));
+});
+
+Breadcrumbs::register('admin.users.edit', function (Crumbs $crumbs, User $user): void {
+    $crumbs->parent('admin.users.show', $user);
+    $crumbs->push('Edit', route('admin.users.edit', $user));
 });
--- api/routes/web.php
+++ api/routes/web.php
@@ -2,19 +2,27 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
 
-Route::get('/', function () {
-    echo 'INIT';
-});
+Auth::routes();
+
+Route::get('/verify/{token}', 'App\Http\Controllers\Auth\RegisterController@verify')->name('register.verify');
+
+Route::get('/cabinet', 'App\Http\Controllers\Cabinet\HomeController@index')->name('cabinet');
+
+Route::group(
+    [
+        'prefix' => 'admin',
+        'as' => 'admin.',
+        'namespace' => 'App\Http\Controllers\Admin',
+        'middleware' => ['auth'],
+    ],
+    static function (): void {
+        Route::get('/', 'HomeController@index')->name('home');
+        Route::resource('users', 'UsersController');
+        Route::post('/users/{user}/verify', 'UsersController@verify')->name('users.verify');
+    }
+);
