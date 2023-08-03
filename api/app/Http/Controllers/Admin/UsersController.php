2a3ce09fc9345c3bc309fcef9b3060292ac02406
frostep <frostell.matters@gmail.com>
2023-08-03 03:27:42 +0300

    admin control users

---
+++ api/app/Http/Controllers/Admin/UsersController.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Users\CreateRequest;
+use App\Http\Requests\Admin\Users\UpdateRequest;
+use App\Models\User\User;
+use App\UseCases\Auth\RegisterService;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UsersController extends Controller
+{
+    private $register;
+
+    public function __construct(RegisterService $register)
+    {
+        $this->register = $register;
+    }
+
+    public function index()
+    {
+        $users = User::orderByDesc('id')->paginate(20);
+
+        return view('admin.users.index', compact('users'));
+    }
+
+    public function create()
+    {
+        return view('admin.users.create');
+    }
+
+    public function store(CreateRequest $request)
+    {
+        $user = User::create($request->only(['name', 'email']) + [
+            'password' => Hash::make(Str::random()),
+            'status' => User::STATUS_ACTIVE,
+        ]);
+
+        return redirect()->route('admin.users.show', $user);
+    }
+
+    public function show(User $user)
+    {
+        return view('admin.users.show', compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        return view('admin.users.edit', compact('user'));
+    }
+
+    public function update(UpdateRequest $request, User $user)
+    {
+        $user->update($request->only(['name', 'email', 'status']));
+
+        return redirect()->route('admin.users.show', $user);
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return redirect()->route('admin.users.index');
+    }
+
+    /**
+     * undocumented function.
+     */
+    public function verify(User $user)
+    {
+        $this->register->verify($user->id);
+
+        return redirect()->route('admin.users.show', $user);
+    }
+}
---
+++ api/app/Http/Requests/Admin/Users/UpdateRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Admin\Users;
+
+use App\Models\User\User;
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * @property User $user
+ */
+class UpdateRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => "required|string|email|max:255|unique:users,email,{$this->user->id},id",
+        ];
+    }
+}
