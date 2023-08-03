1c190c43e0a8c23f8a63a540bde3181ecc300f2e
frostep <frostell.matters@gmail.com>
2023-08-03 03:33:42 +0300

    Auth form users

---
+++ api/resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('breadcrumbs', '')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+
+                <div class="card-header">Hello</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    Your site
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ api/resources/views/text/layout.blade.php
@@ -0,0 +1,9 @@
+{!! strip_tags((string)$header) !!}
+
+{!! strip_tags((string)$slot) !!}
+@isset($subcopy)
+
+{!! strip_tags((string)$subcopy) !!}
+@endisset
+
+{!! strip_tags((string)$footer) !!}
