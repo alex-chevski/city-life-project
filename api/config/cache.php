13a6cd8916622aa15b7e7e069d9dd25899043000
frostep <frostell.matters@gmail.com>
2023-08-24 02:30:46 +0300

    Added redis cache

--- api/config/cache.php
+++ api/config/cache.php
@@ -76,7 +76,8 @@ return [
 
         'redis' => [
             'driver' => 'redis',
-            'connection' => 'cache',
+            // 'connection' => 'cache',
+            'connection' => 'default',
             'lock_connection' => 'default',
         ],
 
